bf759c2b5aa70307f9d820231ff86f5e376b9fe1
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-11-10 00:24:44 +0000

    display user button if there are no users and new user has not been clicked

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
+import React, { useState } from 'react';
+import type { RootState } from '@/redux/store';
+import { useSelector } from 'react-redux';
+
 export default function Home() {
+  const [newUserClicked, setNewUserClicked] = useState(false);
+  const users = useSelector((state: RootState) => state.user.users);
+
   return (
     <main className="h-screen flex items-center justify-center px-4 min-w-[400px]">
       <div className="bg-sky-500 h-[50vh] w-[50%] min-w-[370px] min-h-[400px] rounded-lg flex items-center justify-center">
         <div>
-          <button className="bg-purple-600 px-4 py-2 rounded-md text-xl">New User</button>
+          <button
+            className={`bg-purple-600 px-4 py-2 rounded-md text-xl ${
+              users.length === 0 && newUserClicked === false
+                ? 'inline-block'
+                : 'hidden'
+            }`}
+            onClick={() => setNewUserClicked(true)}
+          >
+            New User
+          </button>
         </div>
       </div>
     </main>
-  )
+  );
 }
