0a71eb627d8656ada8c95a49eddd268a80472867
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-11-10 00:15:40 +0000

    styled new user button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <div>
-        Hello
+    <main className="h-screen flex items-center justify-center px-4 min-w-[400px]">
+      <div className="bg-sky-500 h-[50vh] w-[50%] min-w-[370px] min-h-[400px] rounded-lg flex items-center justify-center">
+        <div>
+          <button className="bg-purple-600 px-4 py-2 rounded-md text-xl">New User</button>
+        </div>
       </div>
     </main>
   )
