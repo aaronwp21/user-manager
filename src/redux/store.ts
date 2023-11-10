e1bf33d4202a76ef9c6edc0ebd384f776a109cd7
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-11-10 00:05:24 +0000

    added user slice to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userSlice from './features/user/userSlice';
 
 export const store = configureStore({
-  reducer: {}
-})
+  reducer: {
+    user: userSlice,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 
-export type AppDispatch = typeof store.dispatch
+export type AppDispatch = typeof store.dispatch;
